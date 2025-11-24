8f01081ab266243fea9f9bf390707dfe8554c0ad
vbudithi <vivek.budithi@gmail.com>
2025-11-25 02:49:06 +1100

    feat(search): add PropertySearchForm and search API endpoint

---
+++ app/api/properties/search/route.js
@@ -0,0 +1,39 @@
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import { NextResponse } from "next/server";
+
+//GET /api/properties/search
+
+export const GET = async (request) => {
+  try {
+    await connectDB();
+    const { searchParams } = new URL(request.url);
+    const location = searchParams.get("location");
+    const propertyType = searchParams.get("propertyType");
+    const locationPattern = new RegExp(location, "i");
+    //Match location pattern against database fields
+    let query = {
+      $or: [
+        { name: locationPattern },
+        { description: locationPattern },
+        { "location.street": locationPattern },
+        { "location.city": locationPattern },
+        { "location.zipcode": locationPattern },
+      ],
+    };
+
+    //only check for property if its not'All'
+    if (propertyType && propertyType !== "All") {
+      const typePattern = new RegExp(propertyType, "i");
+      query.type = typePattern;
+    }
+
+    const properties = await Property.find(query);
+    return NextResponse.json({ properties }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Something went wrong" },
+      { status: 500 }
+    );
+  }
+};
